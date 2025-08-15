0ba69d59b00ef72d303fd6520a675d6575c17e6b
Bimbok <bimbokmkj@gmail.com>
2025-08-15 19:12:43 +0530

    improvments-paths

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const session = require("express-session");
+const path = require("path");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 // Setup session
 app.use(
